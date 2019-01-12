787f9afee65a835c9e6eaaf5fde5876764010638
Luis Zabala <luis.zabala076@gmail.com>
2019-01-12 19:40:10 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,8 +17,7 @@ def first_challenge
 contacts["Freddy mercury"].each do |obj, value|
   if obj == :favorite_icecream_flavors
     value.delete_if do |flavor|
-      
-
+      flavor == "strawberry"
   #remember to return your newly altered contacts hash!
   contacts
 end
