491dbb6e42f50bc304850b9d939d5b3712bd5553
Luis Zabala <luis.zabala076@gmail.com>
2019-01-12 19:31:10 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-contacts[:]
+contacts["Freddy mercury"][favorite_icecream_flavors].shift(0)
 
   #remember to return your newly altered contacts hash!
   contacts
